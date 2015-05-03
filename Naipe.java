f0aafe84f1c22903bd3af84747139e3dc39a72ed
Victor Rodriguez <victor.rodriguez.bahena@intel.com>
2015-05-03 15:14:39 -0500

    Generate file with naipe
    Signed-off-by: Victor Rodriguez <victor.rodriguez.bahena@intel.com>

--- Naipe.java
+++ Naipe.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import javax.swing.ImageIcon;
+import java.io.*;
 
 public class Naipe {
 
@@ -8,8 +9,9 @@ public class Naipe {
 	public static final ImageIcon[] naipesImg=new ImageIcon[52];
 
 	private Random  rnd = new Random();
+    public String BARAJA_FILE = "baraja.txt";
 
-	private int figura=(int)(rnd.nextDouble()*4+0);
+    private int figura=(int)(rnd.nextDouble()*4+0);
 	private int valor=(int)(rnd.nextDouble()*13+0);
 
 	public int getValor(){
@@ -20,29 +22,60 @@ public class Naipe {
             return valor;
         }
 	}
+
 	public String toString(){
 		return valores[valor]+" de "+figuras[figura];
 	}
 
+    public void gen_naipe_file(){
+
+        int pos_naipe = 0;
+        String path = null;
+
+		try {
+
+            PrintWriter writer = new PrintWriter(BARAJA_FILE, "UTF-8");
+
+		    for (int valor = 0; valor< valores.length; valor++){
+		        for (int figura = 0; figura < figuras.length;figura++){
+		            pos_naipe = valor + (figura * 13);
+		            path="Blackjack/Cartas/" + figuras[figura] + "/" + (valor + 1) + ".png";
+		            writer.println(pos_naipe + "," + path);
+		        }
+		    }
+
+		    writer.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+    }
 	public ImageIcon getImage(){
 		String path;
-        int pos_naipe= 0 ; 
+        int pos_naipe= 0 ;
 
         path="Blackjack/Cartas/" + figuras[figura] + "/" + (valor + 1) + ".png";
 
         pos_naipe = valor + (figura * 13);
         naipesImg[pos_naipe]= new ImageIcon(path);
-        
         valor = getValor();
         
         //System.out.println("Randome number:" +valor);
         //System.out.println("Randmob figure:" + figura);
-    	
+
         return naipesImg[pos_naipe];
 	}
 
 	public static void main(String[] args) {
-		Naipe prueba = new Naipe();
+
+        Naipe prueba = new Naipe();
+
+        File f = new File(prueba.BARAJA_FILE);
+        if(f.exists()== false){
+            prueba.gen_naipe_file();
+        }
+
 		System.out.println(prueba.toString());
         System.out.println(prueba.getImage());
 
